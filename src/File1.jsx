d584133c8dd663b282d98af091bde62d15605be9
pareshmore555 <pareshmore555@gmail.com>
2024-09-19 11:09:17 +0530

    update

--- src/File1.jsx
+++ src/File1.jsx
@@ -7,7 +7,7 @@ function File1(){
 
  const [data,setData]=  useState("")
  const [city,setCity]=  useState("")
- 
+ const [color,setColor] =useState("")
 
 
  const apikey="238e63304883da0d74ed9b5de621b1ab";
@@ -18,18 +18,20 @@ function File1(){
     
  }
  console.log(city)
-  console.log(data)
+  
+
+
     return(
-      <div style={{display:"flex",flexDirection:"column", gap:"20px",marginBottom:"400px"}}>
+      <div style={{display:"flex",flexDirection:"column", gap:"20px",marginBottom:"400px"  }}>
         <input type="text" value={city}  style={{ width: '200px', height: '30px' }} onChange={(e)=>setCity(e.target.value)}/>
         <button onClick={handleapi}>Enter</button>
         <div>
           <h2>{data.name}</h2>
-          <p>Main Weather :{data.weather[0].main}</p>
-          <p>Temperature: {data.main.temp}°C</p>
-          <p>Weather: {data.weather[0].description}</p>
-          <p>Temperature Max: {data.main.temp_max}°C</p>
-          <p>Temperature Min: {data.main.temp_min}°C</p>
+         {data.main && <p>Main Weather :{data.weather[0].main}</p>}
+          {data.main && <p>Temperature: {data.main.temp}°C</p>}
+          {data.weather && <p>Weather: {data.weather[0].description}</p>}
+          {data.main &&<p>Temperature Max: {data.main.temp_max}°C</p>}
+          {data.main &&<p>Temperature Min: {data.main.temp_min}°C</p>}
           
         </div>
       </div>
